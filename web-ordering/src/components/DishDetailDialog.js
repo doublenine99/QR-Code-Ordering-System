86860492d998cd8c33c9ffb582d37b65580f3528
jiujiu <pjj851135136@gmail.com>
2019-10-27 21:16:13 -0500

    change to full screen dialog and dispaly the infor from firebase

--- web-ordering/src/components/DishDetailDialog.js
+++ web-ordering/src/components/DishDetailDialog.js
@@ -1,7 +1,6 @@
 // rendering the dialog for the detail information of each dish
 import React, { useEffect } from "react";
 import { withStyles } from "@material-ui/core/styles";
-import Button from "@material-ui/core/Button";
 import Dialog from "@material-ui/core/Dialog";
 import MuiDialogTitle from "@material-ui/core/DialogTitle";
 import MuiDialogContent from "@material-ui/core/DialogContent";
@@ -9,6 +8,13 @@ import MuiDialogActions from "@material-ui/core/DialogActions";
 import IconButton from "@material-ui/core/IconButton";
 import CloseIcon from "@material-ui/icons/Close";
 import Typography from "@material-ui/core/Typography";
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+import { koiSushiRestaurant } from '../Firebase/firebase'
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { useTheme } from '@material-ui/core/styles';
+import { height } from "@material-ui/system";
+import { Divider } from "@material-ui/core";
+import { Box } from '@material-ui/core';
 
 const styles = theme => ({
   root: {
@@ -21,6 +27,7 @@ const styles = theme => ({
     top: theme.spacing(1),
     color: theme.palette.grey[500]
   }
+
 });
 
 const DialogTitle = withStyles(styles)(props => {
@@ -45,6 +52,7 @@ const DialogContent = withStyles(theme => ({
   root: {
     padding: theme.spacing(2)
   }
+
 }))(MuiDialogContent);
 
 const DialogActions = withStyles(theme => ({
@@ -54,50 +62,75 @@ const DialogActions = withStyles(theme => ({
   }
 }))(MuiDialogActions);
 
+
+function handleAddButton(dishRef) {
+  koiSushiRestaurant.collection('tables').doc('t0').collection('cart')
+    .add({
+      dishRef
+    }
+    ).then(ref => {
+      console.log('Added document with ID: ', ref.id);
+    });
+}
+
 export default function DishDetailDialog(props) {
+
+  const theme = useTheme();
+  const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
+
   const [open, setOpen] = React.useState(false);
   useEffect(() => {
-    console.log('count changed', props.open);
+    // console.log('get dish', props.dish);
     if (props.open != null) {
       setOpen(true);
     }
-
   }, [props.open])
-
   const handleClose = () => {
     setOpen(false);
   };
+  if (props.dish != null) {
+    return (
+      <div>
+        <Dialog
+          fullScreen={fullScreen}
+          onClose={handleClose}
+          aria-labelledby="customized-dialog-title"
+          open={open}
+        >
+          <DialogTitle id="customized-dialog-title" onClose={handleClose}>
+            {props.dish.name}
+          </DialogTitle>
+          <DialogContent dividers>
+            <img
+              style={{ width: '350px' }}
+              src={
+                props.dish.image != null ? props.dish.image : "https://firebasestorage.googleapis.com/v0/b/qr-code-ordering-system.appspot.com/o/koisushiMenu%2Fdefault-food-image.jpg?alt=media&token=e6958bef-eae1-4144-b670-e717768d518f"
+              }
+              alt={props.dish.name}
+            />
+            <Typography gutterBottom>
+              {props.dish.description}
+            </Typography>
 
-  return (
-    <div>
-      <Dialog
-        onClose={handleClose}
-        aria-labelledby="customized-dialog-title"
-        open={open}
-      >
-        <DialogTitle id="customized-dialog-title" onClose={handleClose}>
-          Modal title
-        </DialogTitle>
-        <DialogContent dividers>
-          <img
-            src={
-              "https://firebasestorage.googleapis.com/v0/b/qr-code-ordering-system.appspot.com/o/koisushiMenu%2Fxiaolongbao.png?alt=media&token=b4c31508-b269-4d62-8729-27c385993e32"
-            }
-            alt={"xiaolongbao"}
-          // onClick={}
-          />
-          <Typography gutterBottom>
-            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta
-            ac consectetur ac, vestibulum at eros.
-          </Typography>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} color="primary">
-            Save changes
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </div>
-  );
+          </DialogContent>
+          <DialogActions>
+            <Box m={2} fontFamily="Monospace" fontStyle="italic" textAlign="left">
+              <Typography gutterBottom >${props.dish.price}</Typography></Box>
+            <IconButton
+              onClick={() => handleAddButton(props.dish)}
+            >
+              <AddShoppingCartIcon />
+            </IconButton>
+          </DialogActions>
+        </Dialog>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+      </div>
+    )
+  }
+
+
 }
